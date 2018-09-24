adbb44d642b1379c7bfdc549bb73aac2fa0923b3
Ahmed Abdelali <aabdelali@hbku.edu.qa>
2018-09-24 15:48:54 +0300

    Add claimRank link

--- public_html/english/claimrank.php
+++ public_html/english/claimrank.php
@@ -42,18 +42,20 @@ function CallClaimRank($content) {
       $tid = $_GET['tweetid'];
       include 'dbconnection.php';
 
-      $sql="SELECT article FROM tweets_eng WHERE weets_eng.tweet_id=".$tid;
+      $sql="SELECT article FROM tweets_eng WHERE tweets_eng.tweet_id=".$tid;
 
       $result = mysqli_query($conn,$sql);
 
       if ($result->num_rows > 0) 
 
       {
+        $row = $result->fetch_assoc();
         $content = $row['article'];
         #$content = "TRUMP%3A%20Thank%20you%2C%20Lester.%20Our%20jobs%20are%20fleeing%20the%20country.%20They're%20going%20to%20Mexico.%20They're%20going%20to%20many%20other%20countries.%20You%20look%20at%20what%20China%20is%20doing%20to%20our%20country%20in%20terms%20of%20making%20our%20product";
         CallClaimRank($content);
       } else {
-        echo "No content!";
+        echo "<br><h3>No content!</h3><br/>";
+        echo "Query: $sql";
       }
     }
 
