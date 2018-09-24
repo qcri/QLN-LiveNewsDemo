fbd8ba797c2ea0148a651895f2bd00972f8faacf
Ahmed Abdelali <aabdelali@hbku.edu.qa>
2018-09-24 15:52:02 +0300

    Add claimRank link

--- public_html/english/claimrank.php
+++ public_html/english/claimrank.php
@@ -51,11 +51,15 @@ function CallClaimRank($content) {
       {
         $row = $result->fetch_assoc();
         $content = $row['article'];
+        if(length($content)>0)
         #$content = "TRUMP%3A%20Thank%20you%2C%20Lester.%20Our%20jobs%20are%20fleeing%20the%20country.%20They're%20going%20to%20Mexico.%20They're%20going%20to%20many%20other%20countries.%20You%20look%20at%20what%20China%20is%20doing%20to%20our%20country%20in%20terms%20of%20making%20our%20product";
-        CallClaimRank($content);
+         CallClaimRank($content);
+        else {
+          echo "<br><h3>No content!</h3><br/>";
+        }
       } else {
         echo "<br><h3>No content!</h3><br/>";
-        echo "Query: $sql";
+        #echo "Query: $sql";
       }
     }
 
