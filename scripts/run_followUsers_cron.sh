5cce1b211c5a560e9f0bb46c697c6fca0c602a62
Ahmed Abdelali <aabdelali@hbku.edu.qa>
2018-10-10 16:23:06 +0300

    fix cron

--- scripts/run_followUsers_cron.sh
+++ scripts/run_followUsers_cron.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-until ./run_followUsers.sh; do
-	echo "followUsers script crashed! with exit code $?. Respawning.." >&2
-	sleep 1
+export PYTHONPATH=./proppyworker
 
-done
+until python3 followUsers.py -l eng; do
+        echo "followUsers script crashed! with exit code $?. Respawning.." >&2
+        sleep 1
+done &
 
+until python3 followUsers.py -l ara; do
+        echo "followUsers script crashed! with exit code $?. Respawning.." >&2
+        sleep 1
+done &
 
